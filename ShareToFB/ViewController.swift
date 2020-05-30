ab0739273e5829cb4d50c230c30dac18cf5420d3
Sitong Chen <comeonsitong@gmail.com>
2020-05-30 12:57:03 -0300

    Initial Commit

---
+++ ShareToFB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareToFB
+//
+//  Created by Sitong Chen on 2020-05-30.
+//  Copyright © 2020 Sitong Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
